b0a06a79d7fbc3b82a8b703e402fa77dcf66fb9c
notfound404yhkim <kyh31445196@gmail.com>
2023-11-22 12:00:50 +0900

    데이터분석 완료 후 화면개발

---
+++ app_home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def run_home_app():
+    st.subheader('Welcome~~')
+    st.text('이 앱은 고객 데이터 와 자동차 구매 데이터에 대한 내용입니다.')
+    st.text('고객 정보를 넣으면, 차 구매 금액도 예측해 줍니다.')
+
+    img_url = 'https://m.kbchachacha.com/images/cm/theme-hybrid.png'
+    st.image(img_url)
